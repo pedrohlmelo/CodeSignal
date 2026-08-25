c83779107f8ac4f49b038b80122e9f583a9b9dda
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2026-08-25 09:26:24 -0300

    fase 2 com testes ordenacao topN

--- fase2/Main.java
+++ fase2/Main.java
@@ -1,4 +1,4 @@
-package fase1;
+package fase2;
 import java.util.*;
 
 
@@ -23,13 +23,63 @@ public class Main {
     static void addP(String palavra) {
         palavras.add(palavra);
     }
+    public static List<String> topN(Map<String, Integer> freq, int n){
+          List <String> usados = new ArrayList<>();
+          List <String> resultado = new ArrayList<>();
+
+          for(int i =0; i < n; i++){
+            String melhor = null;
+            int melhorN = -1;
+        
+
+            for(String nome : freq.keySet()){
+                if(usados.contains(nome)){
+                    continue;
+                }
+               int qtd = freq.get(nome);
+
+            if(melhor == null){
+                melhor = nome;
+                melhorN = qtd;
+
+            }
+            else if(qtd > melhorN){
+                melhor = nome;
+                melhorN = qtd;
+            }
+
+            else if(qtd == melhorN && nome.compareTo(melhor) < 0){
+                melhor = nome;
+            }
+
+            if(melhor == null){
+                break;
+            }
+
+            }
+            resultado.add(melhor);
+            usados.add(melhor);
+          }
+
+          return resultado;
+
+          
+
+    }
 
     public static void main(String[] args) {
-        addP("Arroz");
-        addP("Arroz");
-        addP("Feijao");
+        //addP("Arroz");
+        //addP("Arroz");
+        //addP("Feijao");
 
-        Map<String, Integer> resultado = frequencia(palavras);
-        System.out.println(resultado);
+        Map<String, Integer> freq = new HashMap<>();
+        freq.put("arroz", 5);
+        freq.put("feijao", 3);
+        freq.put("batata", 5);
+        freq.put("cebola", 1);
+        freq.put("alho", 3);
+        System.out.println(topN(freq,5));
+        //System.out.println(freq);
+
     }
 }
